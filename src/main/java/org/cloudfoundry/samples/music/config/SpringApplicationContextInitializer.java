3153bacf2c839ed621cc1aee2c746b28f62e1416
Elena Neroslavskaya <lenisha@gmail.com>
2019-06-16 21:18:09 -0400

    rm redis, add mvn, app-service

--- src/main/java/org/cloudfoundry/samples/music/config/SpringApplicationContextInitializer.java
+++ src/main/java/org/cloudfoundry/samples/music/config/SpringApplicationContextInitializer.java
@@ -24,7 +24,7 @@ public class SpringApplicationContextInitializer implements ApplicationContextIn
     private static final Log logger = LogFactory.getLog(SpringApplicationContextInitializer.class);
 
     private static final Map<Class<? extends ServiceInfo>, String> serviceTypeToProfileName = new HashMap<>();
-    private static final List<String> validLocalProfiles = Arrays.asList("mysql", "postgres", "mongodb", "redis");
+    private static final List<String> validLocalProfiles = Arrays.asList("mysql", "postgres", "mongodb", "redis", "sqlserver");
 
     public static final String IN_MEMORY_PROFILE = "in-memory";
 
